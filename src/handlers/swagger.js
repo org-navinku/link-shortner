d6ca7c07bfc64cbf421b6fded9ac80f8b8006242
Navin Kumar <navinkumar.cloud@gmail.com>
2025-06-20 14:40:08 +0530

    Update: working serverless link shortener

--- src/handlers/swagger.js
+++ src/handlers/swagger.js
@@ -1,14 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/swagger.json');
-
-// Create a simple Express app to serve Swagger UI
 const express = require('express');
+const path = require('path');
+const swaggerDocument = require('../swagger/swagger.json');
+const serverless = require('serverless-http');
+const swaggerUiDist = require('swagger-ui-dist');
+
 const app = express();
 
-// Serve Swagger UI at /swagger
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Serve Swagger UI static files from swagger-ui-dist
+app.use('/swagger-ui', express.static(swaggerUiDist.getAbsoluteFSPath()));
+
+// Serve Swagger UI at root /
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Swagger UI</title>
+        <link rel="stylesheet" type="text/css" href="/swagger-ui/swagger-ui.css" />
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+        <script src="/swagger-ui/swagger-ui-bundle.js"></script>
+        <script>
+          window.onload = function() {
+            window.ui = SwaggerUIBundle({
+              url: '/swagger.json',
+              dom_id: '#swagger-ui'
+            });
+          };
+        </script>
+      </body>
+    </html>
+  `);
+});
 
 // Serve raw Swagger JSON at /swagger.json
 app.get('/swagger.json', (req, res) => {
@@ -16,6 +40,9 @@ app.get('/swagger.json', (req, res) => {
   res.send(swaggerDocument);
 });
 
-// Export the handler for serverless-http
-const serverless = require('serverless-http');
+// Handle favicon.ico requests
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end();
+});
+
 module.exports.handler = serverless(app);
